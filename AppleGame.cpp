b0554740f75b12802d1718792fa7ea0d7327daa3
Vishal Kumar Kushwaha <123886831+Kushwaha2219@users.noreply.github.com>
2023-10-30 18:32:12 +0530

    Correction in comp random generation

--- AppleGame.cpp
+++ AppleGame.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 #include<stdlib.h>
 #include<time.h>
+#include<conio.h>
 using namespace std;
-int main(){
-	
-	int apple=21,user,comp,turn=0,i;
-	cout<<"*****************************Apple Game****************************"<<endl;
-	cout<<"Rule of the Game:-"<<endl<<"You have 21 apples"<<endl<<"You have to choose from 1 to 4"<<endl;
+void KheloIndia(){
+	int apple=37,user,comp,turn=0,i;
+	srand(time(NULL));
+	cout<<"\t\t\t*****************************Apple Game****************************"<<endl;
+	cout<<"\t\t\t\t\t\tRule of the Game:-"<<endl<<"\t\t\t\t\t\tYou have "<<apple<<" apples"<<endl<<"\t\t\t\t\t   You have to choose from 1 to 4"<<endl;
+	cout<<"\t\t\t**********************************************************"<<endl;
 	while(1){
 		if(turn==0){
+			cout<<endl;
 			cout<<"User Turn:"<<endl;
 			cin>>user;
 			if(user>4||user<=0)
@@ -22,8 +25,10 @@ int main(){
 					cout<<"Apple is less than your input"<<endl;
 				}
 				else{
+					
 					apple-=user;
 					cout<<"Remaining apple is "<<apple<<endl;
+					cout<<endl;
 					turn=1;
 				}
 				
@@ -39,28 +44,25 @@ int main(){
 			
 			
 		}
+		
 		if(turn==1){
-			cout<<"Computer Turn:"<<endl;
-			//comp=5-user;
-			srand(time(NULL));
-			comp=rand()%4+1;
-			cout<<comp<<endl;
 			
-			if(apple<comp){
-				comp=rand()%apple+1;
-			}
-			else{
+			//comp=5-user;
+			
+			comp=rand()%4+1;
+			if(apple>=comp){
+				cout<<"Computer Turn:"<<endl;
+				cout<<comp<<endl;
 				apple-=comp;
 				cout<<"Remaining apple is "<<apple<<endl;
 				turn=0;
 			}
 			
-			
 		}
 		if(apple == 0)
 		{
 			turn = 1;
-		break;
+			break;
 		}
 		
 		else if(apple==1){
@@ -73,7 +75,23 @@ int main(){
 			cout<<"You are looser"<<endl;
 		}
 		else{
-			cout<<"Computer is looser"<<endl;
+			cout<<"Congratulation!! You are Winner"<<endl;
 		}
+}
+int main(){
+	int ch;
+	KheloIndia();
 	
+	while(1){
+		cout<<"Do you want to Play Again--If yes press 1 otherwise Press 0 for exit"<<endl;
+		cin>>ch;
+		if(ch==1){
+			system("cls");
+			KheloIndia();
+		}
+		else{
+			break;
+		}
+	}
+	return 0;
 }
